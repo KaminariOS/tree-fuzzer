29c7fb93428db1fbd9e4b297155a20ad9e4cab24
Kosumi <chankocyo@gmail.com>
2024-04-18 18:40:45 -0400

    Update

--- crates/tree-fuzzer-json-nautilus/fuzz/src/main.rs
+++ crates/tree-fuzzer-json-nautilus/fuzz/src/main.rs
@@ -5,16 +5,47 @@ use tree_fuzzer::fuzz_target;
 
 // #[no_mangle]
 // pub extern "C" fn LLVMFuzzerTestOneInput(data: *const u8, len: usize) 
+//
+//
+fn show_val(val: &Value) {
+    match val {
+        Value::Null => {
+            println!("NULL");
+        },
+        Value::Bool(b) => {
+            println!("bool {b}");
+        },
+        Value::Array(ve) => {
+            ve.iter().for_each(|e| show_val(e));
+        },
+        Value::Number(n) => {
+
+            println!("Number {n}");
+        },
+        Value::String(s) => {
+
+            println!("String {s}");
+        },
+        Value::Object(m) => {
+            m.iter().for_each(|(k, v)| {
+                print!("Key: {k} Val: ");
+                show_val(v);
+            })
+        },
+        
+    }
+}
 
 fuzz_target!(|data| {
     let v = from_slice::<Value>(data);
-    let mut k = [1, 23];
-    let p = k.as_mut_ptr();
-    unsafe {
         // p.add(3).write(5);
-    }
     // println!("FCUKDFSDFSSD");
-    _ = v.map(|js| format!("{}", js.to_string()));
+    if let Ok(val) = v {
+
+        println!("{}", val.to_string())
+    } else {
+        println!("INVALID===================================");
+    }
 });
 
 
